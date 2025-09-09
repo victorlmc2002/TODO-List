e3e733bd0acf38d5ef48944f9bcecd38b0aac09f
Victor Loureiro <victorlmc2002@gmail.com>
2025-09-09 13:57:14 -0300

    Refactor: Melhoria a legibilidade

--- src/App.java
+++ src/App.java
@@ -1,8 +1,4 @@
-import java.util.Scanner;
-
 public class App {
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
         manager.iniciarApp();
